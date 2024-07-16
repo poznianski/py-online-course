705c2caf93cdb5278c060f68a3b5e3a5a2e51e62
Viacheslav Miasoiedov <poznianski@gmail.com>
2024-07-16 23:43:42 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+from typing import Dict
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return math.ceil(days / 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: Dict[str, str | int]) -> "OnlineCourse":
+        return cls(
+            name=course_dict["name"],
+            description=course_dict["description"],
+            weeks=cls.days_to_weeks(course_dict["days"])
+        )
